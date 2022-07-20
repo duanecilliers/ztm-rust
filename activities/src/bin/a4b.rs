238d2a20b1da79d4138bf4111344210f1f7e3244
Duane Cilliers <hello@duane.co.za>
2022-07-20 15:43:10 +0200

    complete activity a4b

--- activities/src/bin/a4b.rs
+++ activities/src/bin/a4b.rs
@@ -10,4 +10,12 @@
 // * Use a match expression to determine which message to display
 // * Use an underscore (_) to match on any value
 
-fn main() {}
+fn main() {
+  let my_value = 3;
+  match my_value {
+    1 => println!("one"),
+    2 => println!("two"),
+    3 => println!("three"),
+    _ => println!("other"),
+  }
+}
