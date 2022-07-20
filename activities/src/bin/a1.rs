9719c0cf4c7a8a50079083a547d9f6f2bd280f99
Duane Cilliers <hello@duane.co.za>
2022-07-20 13:23:43 +0200

    complete exercise 1

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,4 +8,18 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+// * Use a function to display your first name
+fn first_name() {
+  println!("Duane");
+}
+
+// * Use a function to display your last name
+fn last_name() {
+  println!("Cilliers");
+}
+
+// * Use the println macro to display messages to the terminal
+fn main() {
+  first_name();
+  last_name();
+}
