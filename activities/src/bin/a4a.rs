c0b247e80debd6213e7f2cc819af0af4c645050e
Duane Cilliers <hello@duane.co.za>
2022-07-20 15:36:47 +0200

    complete activity a4a

--- activities/src/bin/a4a.rs
+++ activities/src/bin/a4a.rs
@@ -7,4 +7,10 @@
 // * Use a variable set to either true or false
 // * Use a match expression to determine which message to display
 
-fn main() {}
+fn main() {
+  let my_bool = true;
+  match my_bool {
+    true => println!("It's true!"),
+    false => println!("It's false!"),
+  }
+}
