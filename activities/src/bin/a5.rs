9c5b4b6acb6fbdfd11d24c3be8b69274219c68e1
Duane Cilliers <hello@duane.co.za>
2022-07-20 15:52:52 +0200

    complete activity a5

--- activities/src/bin/a5.rs
+++ activities/src/bin/a5.rs
@@ -9,4 +9,13 @@
 // * Print the variable within the loop statement
 // * Use break to exit the loop
 
-fn main() {}
+fn main() {
+  let mut i = 1;
+  loop {
+    if i > 4 {
+      break;
+    }
+    println!("{:?}", i);
+    i += 1;
+  }
+}
