23fdb12a331b07e559e5a256acc5dddd362f623d
Duane Cilliers <hello@duane.co.za>
2022-07-20 15:27:09 +0200

    complete activity a3b

--- activities/src/bin/a3b.rs
+++ activities/src/bin/a3b.rs
@@ -9,4 +9,13 @@
 // * Use an if..else if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+  let x = 5;
+  if x > 5 {
+    println!(">5");
+  } else if x < 5 {
+    println!("<5");
+  } else {
+    println!("=5");
+  }
+}
