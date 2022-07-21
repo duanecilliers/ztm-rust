398592742d50e745edf87aed5e69e1a262b501f5
Duane Cilliers <hello@duane.co.za>
2022-07-21 12:58:16 +0200

    update activity a15

--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -2,7 +2,7 @@
 //
 // Requirements:
 // * Print out a list of tickets and their information for an event
-// * Tickets can be Backstage, Vip, and Standard
+// * Ticket can be Backstage, Vip, and Standard
 // * Backstage and Vip tickets include the ticket holder's name
 // * All tickets include the price
 //
@@ -11,24 +11,24 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-enum Tickets {
-  Backstage(i32, String),
-  VIP(i32, String),
-  Standard(i32)
+enum Ticket {
+  Backstage(f64, String),
+  Vip(f64, String),
+  Standard(f64)
 }
 
 fn main() {
   let tickets = vec![
-    Tickets::Backstage(200, "Duane".to_owned()),
-    Tickets::VIP(150, "Pete".to_owned()),
-    Tickets::Standard(100),
+    Ticket::Backstage(200.0, "Duane".to_owned()),
+    Ticket::Vip(150.0, "Pete".to_owned()),
+    Ticket::Standard(100.0),
   ];
 
   for ticket in tickets {
     match ticket {
-        Tickets::Backstage(price, name) => println!("Price: {:?}, Name: {:?}", price, name),
-        Tickets::VIP(price, name) => println!("Price: {:?}, Name: {:?}", price, name),
-        Tickets::Standard(price) => println!("Price: {:?}", price),
+        Ticket::Backstage(price, name) => println!("Backstage price: {:?}, Name: {:?}", price, name),
+        Ticket::Vip(price, name) => println!("Vip price: {:?}, Name: {:?}", price, name),
+        Ticket::Standard(price) => println!("Standard price: {:?}", price),
     }
   }
 }
