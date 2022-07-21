d98ab99361682c7b05c59b834ec012beccb1b43e
Duane Cilliers <hello@duane.co.za>
2022-07-21 10:14:02 +0200

    attempt activity a12

--- activities/src/bin/a12.rs
+++ activities/src/bin/a12.rs
@@ -10,4 +10,31 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+enum Color {
+  Red,
+  Green,
+  Blue
+}
+
+struct Box {
+  dimensions: (i32, i32, i32), // width, height, depth
+  weight: i32,
+  color: Color
+}
+
+impl Box {
+  fn create_box() -> Self {
+    Self { dimensions: (2, 2, 2), weight: 10, color: Color::Red }
+  }
+
+  fn print_box(&self) {
+    println!("Dimensions: {:?}", self.dimensions);
+    println!("Weight: {:?}", self.weight);
+    // println!("Color: {:?}", self.color);
+  }
+}
+
+fn main() {
+  let my_box = Box::create_box();
+  my_box.print_box();
+}
