03234cd3aaab695d870b7368f23535dc80585ca9
Duane Cilliers <hello@duane.co.za>
2022-07-21 12:15:36 +0200

    complete activity a14

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,28 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+  age: i32,
+  name: String,
+  color: String
+}
+
+fn print(data: &str) {
+  println!("{:?}", data);
+}
+
+
+fn main() {
+  let store = vec![
+    Person { age: 5, name: "Joe".to_owned(), color: "Blue".to_owned() },
+    Person { age: 10, name: "Jane".to_owned(), color: "Red".to_owned() },
+    Person { age: 32, name: "Duane".to_owned(), color: "Purple".to_owned() },
+  ];
+
+  for item in store {
+    if item.age <= 10 {
+      print(&item.name);
+      print(&item.color);
+    }
+  }
+}
