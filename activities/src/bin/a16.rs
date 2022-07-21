8bafd737af89ec53921ad46c455bc17585a561b6
Duane Cilliers <hello@duane.co.za>
2022-07-21 14:25:23 +0200

    complete activity a16

--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -11,4 +11,24 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct Student {
+  name: String,
+  locker: Option<i32>
+}
+
+fn main() {
+  let students = vec![
+    Student { name: "Joe".to_owned(), locker: Some(1) },
+    Student { name: "Jane".to_owned(), locker: None },
+    Student { name: "Pete".to_owned(), locker: Some(2) },
+  ];
+
+  for student in students {
+    match student {
+      Student { name, locker } => match locker {
+        Some(l) => println!("{:?} is assigned locked #{:?}", name, l),
+        None => println!("{:?} is not assigned a locker!", name)
+      }
+    }
+  };
+}
