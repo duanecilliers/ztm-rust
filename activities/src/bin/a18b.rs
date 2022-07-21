a8cad96274e54ec08f2139b723b9491fb1d1a5d5
Duane Cilliers <hello@duane.co.za>
2022-07-21 16:50:37 +0200

    complete activity a18b

--- activities/src/bin/a18b.rs
+++ activities/src/bin/a18b.rs
@@ -22,4 +22,56 @@
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
 
-fn main() {}
+use std::io::Empty;
+
+enum Position {
+    MaintenanceCrew,
+    MarketingDepartment,
+    Manager,
+    LineSupervisor,
+    KitchenStaff,
+    AssemblyTechnician,
+}
+
+enum Status {
+    Active,
+    Terminated
+}
+
+struct Employee {
+    name: String,
+    position: Position,
+    status: Status,
+}
+
+fn try_access(employee: &Employee) -> Result<(), String> {
+    match employee.status {
+        Status::Terminated => return Err("terminated".to_owned()),
+        _ => (),
+    }
+
+    match employee.position {
+        Position::MaintenanceCrew => Ok(()),
+        Position::MarketingDepartment => Ok(()),
+        Position::Manager => Ok(()),
+        _ => Err("invalid position".to_owned())
+    }
+}
+
+fn print_access(employee: &Employee) -> Result<(), String> {
+    let attempt_access = try_access(employee)?;
+    println!("Access granted!");
+    Ok(())
+}
+
+fn main() {
+    let technician = Employee {
+        name: "Joe".to_owned(),
+        position: Position::MaintenanceCrew,
+        status: Status::Active,
+    };
+    match print_access(&technician) {
+        Err(e) => println!("access denied: {:?}", e),
+        _ => ()
+    }
+}
