fd87329af749d78976032377662decec3fb3febc
Duane Cilliers <hello@duane.co.za>
2022-07-22 16:11:45 +0200

    add main menu and bill structs with impl

--- activities/src/bin/p1.rs
+++ activities/src/bin/p1.rs
@@ -31,6 +31,30 @@
 
 use std::io;
 
+#[derive(Debug, Clone)]
+pub struct Bill {
+    name: String,
+    amount: f64,
+}
+
+pub struct Bills {
+    inner: Vec<Bill>,
+}
+
+impl Bills {
+    fn new() -> Self {
+        Self { inner: vec![] }
+    }
+
+    fn add(&mut self, bill: Bill) {
+        self.inner.push(bill);
+    }
+
+    fn get_all(&self) -> Vec<&Bill> {
+        self.inner.iter().collect()
+    }
+}
+
 fn get_input() -> Option<String> {
     let mut buffer = String::new();
     while io::stdin().read_line(&mut buffer).is_err() {
@@ -44,7 +68,40 @@ fn get_input() -> Option<String> {
     }
 }
 
+#[derive(Debug)]
+enum MainMenu {
+    AddBill,
+    ViewBill,
+}
+
+impl MainMenu {
+    fn from_str(input: &str) -> Option<MainMenu> {
+        match input {
+            "1" => Some(Self::AddBill),
+            "2" => Some(Self::ViewBill),
+            _ => None,
+        }
+    }
+
+    fn show() {
+        println!("");
+        println!("== Bill Manager ==");
+        println!("");
+        println!("1. Add Bill");
+        println!("2. View Bills");
+        println!("");
+        println!("Enter selection: ");
+    }
+}
+
 fn main() {
-    let input = get_input();
-    println!("input: {:?}", input)
+    loop {
+        MainMenu::show();
+        let input = get_input().expect("no data entered");
+        match MainMenu::from_str(input.as_str()) {
+            Some(MainMenu::AddBill) => (),
+            Some(MainMenu::ViewBill) => (),
+            None => return,
+        }
+    }
 }
