57fe4e007142f6bff26438b491fde9f47708f7b6
Duane Cilliers <hello@duane.co.za>
2022-07-22 12:59:20 +0200

    complete activity a20

--- activities/src/bin/a20.rs
+++ activities/src/bin/a20.rs
@@ -23,4 +23,56 @@
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
 
-fn main() {}
+use std::io;
+
+enum PowerState {
+    Off,
+    Sleep,
+    Reboot,
+    Shutdown,
+    Hibernate,
+}
+
+fn get_input() -> io::Result<String> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    Ok(buffer.trim().to_owned())
+}
+
+fn print_msg(state: PowerState) {
+    match state {
+        PowerState::Off => println!("turning off"),
+        PowerState::Sleep => println!("going to sleep"),
+        PowerState::Reboot => println!("rebooting system"),
+        PowerState::Shutdown => println!("shutting down"),
+        PowerState::Hibernate => println!("Going into hybernation"),
+    }
+}
+
+fn map_input(input: String) -> Result<PowerState, String> {
+    match input.to_lowercase().as_str() {
+        "off" => Ok(PowerState::Off),
+        "sleep" => Ok(PowerState::Sleep),
+        "reboot" => Ok(PowerState::Reboot),
+        "shutdown" => Ok(PowerState::Shutdown),
+        "hibernate" => Ok(PowerState::Hibernate),
+        _ => Err("invalid command, please select one of 'Off', 'Sleep', 'Reboot', 'Shutdown', 'Hibernate'"
+            .to_owned()),
+    }
+}
+
+fn main() {
+    let mut received_command = false;
+    while !received_command {
+        match get_input() {
+            Ok(input) => match map_input(input) {
+                Ok(state) => {
+                    received_command = true;
+                    print_msg(state)
+                }
+                Err(e) => println!("error: {:?}", e),
+            },
+            Err(e) => println!("error: {:?}", e),
+        }
+    }
+}
