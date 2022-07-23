a2c39de2b362828cdc30c966ba6b455fa218f482
Duane Cilliers <hello@duane.co.za>
2022-07-23 12:33:02 +0200

    complete activity a33

--- activities/src/bin/a33.rs
+++ activities/src/bin/a33.rs
@@ -8,8 +8,17 @@
 // * No data may be copied (cannot use .to_owned() or .to_string())
 // * If both strings are the same length, the first one should be returned
 
+fn longest<'a>(str1: &'a str, str2: &'a str) -> &'a str {
+    if str1.len() == str2.len() {
+        return str1;
+    } else if str1.len() > str2.len() {
+        return str1;
+    } else {
+        return str2;
+    }
+}
 fn main() {
     let short = "hello";
     let long = "this is a long message";
-    println!("{}", longest(short, long))
+    println!("{}", longest(&short, &long))
 }
