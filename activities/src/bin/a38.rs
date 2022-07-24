28bf801be6b610cc5dd37d857a1b7ddd36f3c19e
Duane Cilliers <hello@duane.co.za>
2022-07-24 12:17:30 +0200

    complete activity a38

--- activities/src/bin/a38.rs
+++ activities/src/bin/a38.rs
@@ -8,6 +8,8 @@
 // Notes:
 // * Use the join function to wait for threads to finish
 
+use std::thread;
+
 fn msg_hello() -> &'static str {
     use std::time::Duration;
     std::thread::sleep(Duration::from_millis(1000));
@@ -26,4 +28,22 @@ fn msg_excited() -> &'static str {
     "!"
 }
 
-fn main() {}
+fn main() {
+    let hello_handle = thread::spawn(move || msg_hello());
+    let thread_handle = thread::spawn(move || msg_thread());
+    let excited_handle = thread::spawn(move || msg_excited());
+
+    println!("Waiting for threads...");
+    match hello_handle.join() {
+        Ok(msg) => println!("{:?}", msg),
+        Err(e) => println!("hello error: {:?}", e),
+    };
+    match thread_handle.join() {
+        Ok(msg) => println!("{:?}", msg),
+        Err(e) => println!("thread error: {:?}", e),
+    };
+    match excited_handle.join() {
+        Ok(msg) => println!("{:?}", msg),
+        Err(e) => println!("excited error: {:?}", e),
+    };
+}
