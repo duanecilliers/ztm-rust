102e8df00f176b0b903e43dedfe754c23f856773
Duane Cilliers <hello@duane.co.za>
2022-07-25 21:24:25 +0200

    implement data DbId module

---
+++ clipstash/src/lib/data/mod.rs
@@ -0,0 +1,30 @@
+use derive_more::{Display, From};
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+use uuid::Uuid;
+
+#[derive(Clone, Debug, From, Display, Deserialize, Serialize)]
+pub struct DbId(Uuid);
+
+impl DbId {
+    pub fn new() -> Self {
+        Uuid::new_v4().into()
+    }
+
+    pub fn nil() -> DbId {
+        Self(Uuid::nil())
+    }
+}
+
+impl Default for DbId {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl FromStr for DbId {
+    type Err = uuid::Error;
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        Ok(DbId(Uuid::parse_str(id)?))
+    }
+}
