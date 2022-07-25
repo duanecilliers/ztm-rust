2255d4e7cf9e5b47bb8292293a9eac3b5f91529d
Duane Cilliers <hello@duane.co.za>
2022-07-25 21:19:17 +0200

    implement posted field module

---
+++ clipstash/src/lib/domain/clip/field/posted.rs
@@ -0,0 +1,13 @@
+use crate::domain::clip::ClipError;
+use crate::domain::time::Time;
+use derive_more::Constructor;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Constructor, Debug, Deserialize, Serialize)]
+pub struct Posted(Time);
+
+impl Posted {
+    pub fn into_inner(self) -> Time {
+        self.0
+    }
+}
