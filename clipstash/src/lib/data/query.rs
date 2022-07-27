3617ca43ec6e9c47ad2e63afe067e6a46c219c4d
Duane Cilliers <hello@duane.co.za>
2022-07-27 13:06:09 +0200

    update model and query with new clip

--- clipstash/src/lib/data/query.rs
+++ clipstash/src/lib/data/query.rs
@@ -19,3 +19,33 @@ pub async fn get_clip<M: Into<model::GetClip>>(
     .fetch_one(pool)
     .await?)
 }
+
+pub async fn new_clip<M: Into<model::NewClip>>(
+    model: M,
+    pool: &DatabasePool,
+) -> Result<model::Clip> {
+    let model = model.into();
+    let _ = sqlx::query!(
+        r#"INSERT INTO CLIPS (
+          clip_id,
+          shortcode,
+          content,
+          title,
+          posted,
+          expires,
+          password,
+          hits)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)"#,
+        model.clip_id,
+        model.shortcode,
+        model.content,
+        model.title,
+        model.posted,
+        model.expires,
+        model.password,
+        0
+    )
+    .execute(pool)
+    .await?;
+    get_clip(model.shortcode, pool).await
+}
