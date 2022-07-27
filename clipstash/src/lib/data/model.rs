9c8c7207f0e00a6cfa4cb1008ea5f9bd0cb60684
Duane Cilliers <hello@duane.co.za>
2022-07-27 13:10:25 +0200

    update model and query with update clip

--- clipstash/src/lib/data/model.rs
+++ clipstash/src/lib/data/model.rs
@@ -60,3 +60,11 @@ pub struct NewClip {
     pub(in crate::data) expires: Option<NaiveDateTime>,
     pub(in crate::data) password: Option<String>,
 }
+
+pub struct UpdateClip {
+    pub(in crate::data) shortcode: String,
+    pub(in crate::data) content: String,
+    pub(in crate::data) title: Option<String>,
+    pub(in crate::data) expires: Option<NaiveDateTime>,
+    pub(in crate::data) password: Option<String>,
+}
