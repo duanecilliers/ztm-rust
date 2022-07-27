8f9ff64a0f163aaef59790a76cae74241b1a96de
Duane Cilliers <hello@duane.co.za>
2022-07-27 18:08:08 +0200

    add rocket config

--- clipstash/src/lib/mod.rs
+++ clipstash/src/lib/mod.rs
@@ -3,8 +3,27 @@ pub mod domain;
 pub mod service;
 pub mod web;
 
+use data::AppDatabase;
 pub use data::DataError;
 pub use domain::clip::field::ShortCode;
 pub use domain::clip::{Clip, ClipError};
 pub use domain::time::Time;
 pub use service::ServiceError;
+use web::renderer::Renderer;
+
+use rocket::fs::FileServer;
+use rocket::{Build, Rocket};
+
+pub fn rocket(config: RocketConfig) -> Rocket<Build> {
+    rocket::build()
+        .manage::<AppDatabase>(config.database)
+        .manage::<Renderer>(config.renderer)
+        .mount("/", web::http::routes())
+        .mount("/static", FileServer::from("static"))
+        .register("/", web::http::catcher::catchers())
+}
+
+pub struct RocketConfig {
+    pub renderer: Renderer<'static>,
+    pub database: AppDatabase,
+}
